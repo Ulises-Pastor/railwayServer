a20c3dbba1ec2109e7a3bc1b123c469bdf39786b
Ulises-Pastor <pamu010914@gs.utm.mx>
2023-12-11 03:10:12 -0600

    Second commit

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Blink',
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DA_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'Blink',
         ssl: true
     }
 };
